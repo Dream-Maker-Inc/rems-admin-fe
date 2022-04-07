e63802727873e7e06a97ff23a8e24b1455b98192
NightOwl <steelman2019e@gmail.com>
2022-04-07 20:43:28 +0900

    fix: 로그인 뷰 height max

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "@mui/material/styles";
+import { LoginView } from "./components/PC/Login/LoginView";
 import { lightTheme } from "./themes/CustomThemes";
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
-      <main>main</main>
+      <LoginView />
     </ThemeProvider>
   );
 }
--- src/components/PC/Login/LoginView/LoginView.jsx
+++ src/components/PC/Login/LoginView/LoginView.jsx
@@ -7,8 +7,10 @@ export const LoginView = () => {
   return (
     <Stack
       component={"article"}
+      justifyContent={"center"}
       alignItems={"center"}
       minWidth={"1280px"}
+      height={"100%"}
       spacing={"48px"}
       padding={"120px"}
       bgcolor={"#2f3539"}
