28723ff251bc197adf80f0eecfe2ffaa33e7173d
NightOwl <steelman2019e@gmail.com>
2022-04-11 16:36:16 +0900

    fix: 사용 동의 - 메인 레이아웃 제거

--- src/components/PC/UsingTerm/UsingTermView/UsingTermView.jsx
+++ src/components/PC/UsingTerm/UsingTermView/UsingTermView.jsx
@@ -21,50 +21,40 @@ export const UsingTermView = () => {
   const handleCancelClick = () => alert("todo 취소");
 
   return (
-    <MainTemplate>
-      <Stack padding={"24px"}>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox />}
-            label="전체 동의"
-            value={agreeAll}
-            onChange={(e) => handleAgreeAllCheckChange(e.target.value)}
-          />
-          <FormControlLabel
-            control={<Checkbox />}
-            label="사용 동의"
-            value={agree}
-            onChange={(e) => handleAgreeCheckChange(e.target.value)}
-          />
-        </FormGroup>
+    <Stack width={"100%"} height={"100%"} p={"24px"} bgcolor={"#303539"}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox />}
+          label="전체 동의"
+          value={agreeAll}
+          onChange={(e) => handleAgreeAllCheckChange(e.target.value)}
+        />
+        <FormControlLabel
+          control={<Checkbox />}
+          label="사용 동의"
+          value={agree}
+          onChange={(e) => handleAgreeCheckChange(e.target.value)}
+        />
+      </FormGroup>
 
-        <Stack mt={"16px"}>
-          <TextField
-            multiline
-            InputProps={{ readOnly: true, sx: { opacity: 0.7 } }}
-            rows={10}
-            defaultValue={"어플리케이션을 사용하는데 동의합니다."}
-          />
-        </Stack>
-
-        <ButtonSection>
-          <Button
-            variant={"contained"}
-            size={"small"}
-            onClick={handleAgreeClick}
-          >
-            동의
-          </Button>
-          <Button
-            variant={"outlined"}
-            size={"small"}
-            onClick={handleCancelClick}
-          >
-            취소
-          </Button>
-        </ButtonSection>
+      <Stack mt={"16px"}>
+        <TextField
+          multiline
+          InputProps={{ readOnly: true, sx: { opacity: 0.7 } }}
+          rows={10}
+          defaultValue={"어플리케이션을 사용하는데 동의합니다."}
+        />
       </Stack>
-    </MainTemplate>
+
+      <ButtonSection>
+        <Button variant={"contained"} size={"small"} onClick={handleAgreeClick}>
+          동의
+        </Button>
+        <Button variant={"outlined"} size={"small"} onClick={handleCancelClick}>
+          취소
+        </Button>
+      </ButtonSection>
+    </Stack>
   );
 };
 
