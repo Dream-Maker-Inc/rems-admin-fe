e7b451bb07350dd302c5c198195c6b83d7a1e6f6
NightOwl <steelman2019e@gmail.com>
2022-04-11 16:34:17 +0900

    fix: laptop menu 3depth 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { ThemeProvider } from "@mui/material/styles";
-import { UsingTermView } from "./components/Mobile/UsingTerm/UsingTermView/UsingTermView";
 import { GroupSelectView } from "./components/PC/GroupSelect/GroupSelectView";
 import { laptopTheme, mobileTheme } from "./themes/CustomThemes";
 import { useCustomMediaQuery } from "./themes/UseCustomMediaQuery";
