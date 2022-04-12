c17d75a370a12105978056e8a9c23a71717cadae
NightOwl <steelman2019e@gmail.com>
2022-04-12 19:02:37 +0900

    fix: [PC] 사이드 menu 클릭 시 dom 프로퍼티 에러

--- src/components/PC/Main/MainDrawer/elements/Menus/Menus.jsx
+++ src/components/PC/Main/MainDrawer/elements/Menus/Menus.jsx
@@ -8,79 +8,79 @@ const Depth2PaddingLeft = 48;
 const SubDepthPaddingLeft = 24;
 
 export const Menus = () => {
-  const menuData = MenusModel;
-  const handleMenuClick = (title) => alert(`title: ${title}`);
+    const menuData = MenusModel;
+    const handleMenuClick = (title) => alert(`title: ${title}`);
 
-  const renderTree = (model, paddingLeft) =>
-    model.children &&
-    model.children.map((it) => (
-      <StyledTreeItem
-        key={it.id}
-        nodeId={it.id}
-        label={it.title}
-        paddingLeft={paddingLeft}
-      >
-        {renderTree(it, paddingLeft + SubDepthPaddingLeft)}
-      </StyledTreeItem>
-    ));
+    const renderTree = (model, paddingLeft) =>
+        model.children &&
+        model.children.map((it) => (
+            <StyledTreeItem
+                key={it.id}
+                nodeId={it.id}
+                label={it.title}
+                paddingleft={paddingLeft}
+            >
+                {renderTree(it, paddingLeft + SubDepthPaddingLeft)}
+            </StyledTreeItem>
+        ));
 
-  return (
-    <StyledTreeView
-      defaultCollapseIcon={<ExpandMoreRounded />}
-      defaultExpandIcon={<ExpandLessRounded />}
-      onNodeSelect={(_, v) => handleMenuClick(v)}
-    >
-      {menuData.map((it) => (
-        <StyledTreeHead
-          key={it.id}
-          nodeId={it.id}
-          label={
-            <TreeHeaderLabelWithIcon
-              icon={it.icon}
-              children={<Typography>{it.title}</Typography>}
-            />
-          }
+    return (
+        <StyledTreeView
+            defaultCollapseIcon={<ExpandMoreRounded />}
+            defaultExpandIcon={<ExpandLessRounded />}
+            onNodeSelect={(_, v) => handleMenuClick(v)}
         >
-          {renderTree(it, Depth2PaddingLeft)}
-        </StyledTreeHead>
-      ))}
-    </StyledTreeView>
-  );
+            {menuData.map((it) => (
+                <StyledTreeHead
+                    key={it.id}
+                    nodeId={it.id}
+                    label={
+                        <TreeHeaderLabelWithIcon
+                            icon={it.icon}
+                            children={<Typography>{it.title}</Typography>}
+                        />
+                    }
+                >
+                    {renderTree(it, Depth2PaddingLeft)}
+                </StyledTreeHead>
+            ))}
+        </StyledTreeView>
+    );
 };
 
 const StyledTreeView = styled(TreeView)`
-  color: #fff;
+    color: #fff;
 
-  & .MuiCollapse-root {
-    margin: 0;
-  }
+    & .MuiCollapse-root {
+        margin: 0;
+    }
 `;
 
 const StyledTreeHead = styled(TreeItem)`
-  border-top: 1px solid #eeeeee33;
-  background-color: #2d3034;
+    border-top: 1px solid #eeeeee33;
+    background-color: #2d3034;
 
-  & .MuiTreeItem-content {
-    flex-direction: row-reverse;
-    padding: 12px 16px;
-  }
+    & .MuiTreeItem-content {
+        flex-direction: row-reverse;
+        padding: 12px 16px;
+    }
 `;
 
 const StyledTreeItem = styled(TreeItem)`
-  background-color: #232629;
+    background-color: #232629;
 
-  & .MuiTreeItem-content {
-    padding-left: ${({ paddingLeft }) => `${paddingLeft}px !important`};
-  }
+    & .MuiTreeItem-content {
+        padding-left: ${({ paddingleft }) => `${paddingleft}px !important`};
+    }
 
-  & .MuiTreeItem-label::before {
-    content: "- ";
-  }
+    & .MuiTreeItem-label::before {
+        content: "- ";
+    }
 `;
 
 const TreeHeaderLabelWithIcon = ({ icon: iconPath, children }) => (
-  <Stack direction={"row"} spacing={"8px"}>
-    <img src={iconPath} alt="icon" />
-    {children}
-  </Stack>
+    <Stack direction={"row"} spacing={"8px"}>
+        <img src={iconPath} alt="icon" />
+        {children}
+    </Stack>
 );
