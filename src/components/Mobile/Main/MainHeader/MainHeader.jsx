1d82e1afbfd98717e9c7933280634e637b1e1ded
NightOwl <steelman2019e@gmail.com>
2022-04-13 18:43:15 +0900

    fix: [M] GNB 패딩 값 조정

--- src/components/Mobile/Main/MainHeader/MainHeader.jsx
+++ src/components/Mobile/Main/MainHeader/MainHeader.jsx
@@ -4,40 +4,45 @@ import { AppBar, IconButton, Stack, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
 
 export const MainHeader = () => {
-  const title = "화면레이아웃";
+    const title = "화면레이아웃";
 
-  // 사이드 메뉴 토글
-  const [openSide, setOpenSide] = useState(false);
-  const handleSideMenuToggle = () => setOpenSide((old) => !old);
+    // 사이드 메뉴 토글
+    const [openSide, setOpenSide] = useState(false);
+    const handleSideMenuToggle = () => setOpenSide((old) => !old);
 
-  // refresh
-  const handleRefresh = () => alert("refresh");
+    // refresh
+    const handleRefresh = () => alert("refresh");
 
-  return (
-    <AppBar position="static" elevation={1}>
-      <Toolbar
-        sx={{ bgcolor: "primary.main", borderBottom: "1px solid #eeeeee33" }}
-      >
-        <Inner>
-          <IconButton onClick={handleSideMenuToggle}>
-            <MenuRounded htmlColor={"#fff"} />
-          </IconButton>
+    return (
+        <AppBar position="static" elevation={1}>
+            <Toolbar
+                disableGutters
+                sx={{
+                    bgcolor: "primary.main",
+                    borderBottom: "1px solid #eeeeee33",
+                    px: "8px",
+                }}
+            >
+                <Inner>
+                    <IconButton onClick={handleSideMenuToggle}>
+                        <MenuRounded htmlColor={"#fff"} />
+                    </IconButton>
 
-          <Typography variant="h6">{title}</Typography>
+                    <Typography variant="h6">{title}</Typography>
 
-          <IconButton onClick={handleRefresh}>
-            <RefreshRounded htmlColor={"#fff"} />
-          </IconButton>
-        </Inner>
-      </Toolbar>
-    </AppBar>
-  );
+                    <IconButton onClick={handleRefresh}>
+                        <RefreshRounded htmlColor={"#fff"} />
+                    </IconButton>
+                </Inner>
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 const Inner = styled(Stack)`
-  position: relative;
-  width: 100%;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+    position: relative;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
 `;
